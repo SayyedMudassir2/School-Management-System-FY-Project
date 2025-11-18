32e922d298f7e34ee19b83c541787e6d698a766a
Sayyed Mudassir <sayyedmudassir443@gmail.com>
2025-11-18 18:12:46 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,8 @@
-
-// src/firebase/config.ts
-import { FirebaseOptions } from 'firebase/app';
-
-// This is a placeholder for your Firebase config.
-// In a real application, you would replace this with your actual
-// Firebase project configuration.
-export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyCpRLAVDd2gVNRttoJ37VE5youfYNg8oqs",
-  authDomain: "studio-8530753944-20f5c.firebaseapp.com",
-  projectId: "studio-8530753944-20f5c",
-  storageBucket: "studio-8530753944-20f5c.appspot.com",
-  messagingSenderId: "238674371362",
-  appId: "1:238674371362:web:7d9a9e6f5cd61fb1c17b7b",
+export const firebaseConfig = {
+  "projectId": "studio-8530753944-20f5c",
+  "appId": "1:238674371362:web:7d9a9e6f5cd61fb1c17b7b",
+  "apiKey": "AIzaSyCpRLAVDd2gVNRttoJ37VE5youfYNg8oqs",
+  "authDomain": "studio-8530753944-20f5c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "238674371362"
 };
