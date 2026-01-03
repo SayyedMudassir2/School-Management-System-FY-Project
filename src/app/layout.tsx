d6f559b8618c0d750fa2b3a234019ce099380956
Sayyed Mudassir <sayyedmudassir443@gmail.com>
2026-01-03 05:55:39 +0000

    11:21:54.325 Running build in Washington, D.C., USA (East) – iad1 11:21:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,35 @@
+
+'use client';
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from '@/lib/utils';
 import { Analytics } from '@vercel/analytics/react';
 import { ProgressBar } from '@/components/ui/progress-bar';
+import { useEffect, useState } from 'react';
 
-export const metadata: Metadata = {
-  title: 'Aedura - Elite School Management',
-  description: "Aedura is our school’s centralized, all-in-one digital system.",
-};
+// export const metadata: Metadata = {
+//   title: 'Aedura - Elite School Management',
+//   description: "Aedura is our school’s centralized, all-in-one digital system.",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <html lang="en" className="dark" style={{ colorScheme: 'dark' }} suppressHydrationWarning>
       <head>
+        <title>Aedura - Elite School Management</title>
+        <meta name="description" content="Aedura is our school’s centralized, all-in-one digital system." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;800&display=swap" rel="stylesheet" />
@@ -25,7 +37,7 @@ export default function RootLayout({
       <body className={cn(
         "font-sans antialiased"
       )} suppressHydrationWarning>
-        <ProgressBar />
+        {isClient && <ProgressBar />}
         {children}
         <Toaster />
         <Analytics />
